d60e5e8aee1961bdebe3ff00debd3b1e6621c0d1
ACM-Anik <anikmojumder5252@gmail.com>
2023-12-20 04:05:36 +0600

    changed carousel from react-bootstrap to normal and changed some bg

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './home.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
@@ -7,8 +7,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from '../../assets';
 import Image1 from '../../assets/maick-maciel-_v8C6BlpBzY-unsplash.jpg';
 import Image2 from '../../assets/tim-gouw-tYpp-eIZH44-unsplash.jpg';
 import Image3 from '../../assets/pavan-gupta-_HzlOHmboSk-unsplash.jpg';
@@ -18,25 +16,21 @@ import Card from 'react-bootstrap/Card';
 
 function Home() {
 
-    useEffect(() => {
-
-    }, []);
-
     return (
         <>
             {/* Navbarr:--- */}
-            <Navbar expand="lg" className="bg-body-tertiary nav-bg">
-                <Container fluid>
+            <Navbar expand="lg" className=" nav-bg">
+                <Container fluid className=" text-white">
                     <Navbar.Brand href="/">FPO Agro</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
-                            className="me-auto my-2 my-lg-0"
+                            className="me-auto my-2 my-lg-0 text-white"
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
                             <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <Nav.Link href="#action2">About</Nav.Link>
                             <NavDropdown title="Link" id="navbarScrollingDropdown">
                                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action4">
@@ -47,9 +41,6 @@ function Home() {
                                     Something else here
                                 </NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/" disabled>
-                                Link
-                            </Nav.Link>
                         </Nav>
                         <Form className="d-flex" id="search-field">
                             <Form.Control
@@ -66,61 +57,68 @@ function Home() {
 
 
             {/* Carousel:--- */}
-            <div className="carousel-cont">
-                <Carousel>
-                    <Carousel.Item>
-                        <img src={Image1} alt="First slide" className="carousel-img" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={Image2} alt="Second slide" className="carousel-img" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={Image3} alt="Third slide" className="carousel-img" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+            <div id="carouselExampleAutoplaying" className="carousel slide position-relative" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={Image1} className="carousel-img d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Image2} className="carousel-img d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Image3} className="carousel-img d-block w-100" alt="..." />
+                    </div>
+                </div>
+
+                {/* <input type="text" className="w-25 position absolute bottom-50 end-50"/> */}
+
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
 
 
 
             {/* Cards ---: */}
             <div>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <h1 className="text-center fw-bold mt-5">Top Nominated Farmer</h1>
+
+                <div className="d-flex justify-content-center gap-5 align-items-center mt-4">
+                    <Card style={{ width: '15rem', borderRadius: '50%' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" className="w-25" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="success" size="sm">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '15rem', borderRadius: '50%' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" className="w-25" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="success" size="sm">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
             </div>
 
 
 
             <div className="mt-5">
-                <Button variant="primary">Primary</Button>{' '}
-                <Button variant="secondary">Secondary</Button>{' '}
-                <Button variant="success">Success</Button>{' '}
-                <Button variant="warning">Warning</Button>{' '}
-                <Button variant="danger">Danger</Button>{' '}
-                <Button variant="info">Info</Button>{' '}
-                <Button variant="light">Light</Button>{' '}
-                <Button variant="dark">Dark</Button>
-                <Button variant="link">Link</Button>
+                
             </div>
 
         </>
